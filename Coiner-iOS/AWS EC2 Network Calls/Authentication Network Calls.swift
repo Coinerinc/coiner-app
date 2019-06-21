caaeaa77651c297bb6f49a5935e938bbd73a893f
JFurstoss <furstoss.justin@gmail.com>
2019-06-21 11:02:46 -0400

    final Push before exporting to COINERAPP Repo

---
+++ Coiner-iOS/AWS EC2 Network Calls/Authentication Network Calls.swift
@@ -0,0 +1,9 @@
+//
+//  Authentication Network Calls.swift
+//  Coiner-iOS
+//
+//  Created by Justin Furstoss on 6/21/19.
+//  Copyright © 2019 Justin Furstoss. All rights reserved.
+//
+
+import Foundation
